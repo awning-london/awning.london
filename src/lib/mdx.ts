5ea87c1781a02fd06ac46c5df2b6650c0ee69ebf
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-11-09 10:11:37 +0330

    update component

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -1,3 +1,5 @@
+// "use client"
+
 import { type ImageProps } from 'next/image'
 import glob from 'fast-glob'
 
@@ -9,7 +11,7 @@ async function loadEntries<T extends { date: string }>(
     await Promise.all(
       (await glob('**/page.mdx', { cwd: `src/app/${directory}` })).map(
         async (filename) => {
-          let metadata = (await import(`../app/${directory}/${filename}`))[
+          const metadata = (await import(`../app/${directory}/${filename}`))[
             metaName
           ] as T
           return {
