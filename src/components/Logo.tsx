595a9cc00690798c438d83756833d5ab1bd9bb2f
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-11-09 10:53:03 +0330

    responsive logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,9 +6,10 @@ export default function LogoV2() {
   return (
     <Image
         src={Logo}
-        width={60}
-        height={60}
+        width={40}
+        height={40}
         alt='logo of the awning'
+        className='md:w-14'
     />
   )
 }
