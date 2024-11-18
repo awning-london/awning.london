e937ea1d8fdcd47e6f4e3dde86fa2caa67831067
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-11-18 17:05:46 +0330

    fix (contact form layout)

--- src/app/contact/contact-form.tsx
+++ src/app/contact/contact-form.tsx
@@ -8,11 +8,8 @@ export function ContactForm() {
     return (
       <FadeIn className="lg:order-last">
         <iframe data-tally-src="https://tally.so/embed/np0Ab8?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="276"  title="Contact form
-        ">
-  
-        </iframe>
+        "></iframe>
         
-      
       <Script
           id="tally-js"
           src="https://tally.so/widgets/embed.js"
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -82,8 +82,8 @@ export default function Contact() {
       </PageIntro>
 
       <Container className="mt-24 sm:mt-32 lg:mt-40">
+        <ContactForm />
         <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
-          <ContactForm />
           <ContactDetails />
         </div>
       </Container>
