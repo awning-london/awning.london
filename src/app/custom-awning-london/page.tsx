fb0353530055a676d9d64e65027beb3c0e6be3e5
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-02-14 22:27:14 +0330

    update custom content

--- src/app/custom-awning-london/page.tsx
+++ src/app/custom-awning-london/page.tsx
@@ -1,20 +1,108 @@
-import { FadeIn } from '@/components/FadeIn'
+import { Container } from '@/components/Container'
+import { FadeIn, FadeInStagger } from '@/components/FadeIn'
+import clsx from 'clsx'
 import { type Metadata } from 'next'
-
+import Image from 'next/image'
 
 
 export const metadata: Metadata = {
-  title: 'Contact Us | Awning London',
-  description: 'Let’s work together. We can’t wait to hear from you. Get in touch with Awning London for all your awning needs.',
+  title: 'Custom Canopy | Awning London',
+  description: 'Enhance your property’s curb appeal with our premium custom awnings in London. ',
 }
 
+const CustomFeatures = [
+  ['Transform Your Outdoor Spaces with Bespoke Design'],
+  ['Tailored to Your Unique Style'],
+  ['Premium Quality Materials & Craftsmanship'],
+  ['Expert Installation & Seamless Integration'],
+]
+
+const CustomContent = [
+  {
+    "id": 1,
+    "title": "Transform Your Outdoor Spaces with Bespoke Design",
+    "desc": "Enhance your home’s curb appeal with our premium custom awnings in London. We offer tailor-made solutions, from design consultations to expert installation, ensuring your awning complements your home or business perfectly.",
+    "imgsrc": "/images/img9.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Tailored to Your Style",
+    "desc": "Our bespoke awnings reflect your unique aesthetic, blending modern and classic design elements for a one-of-a-kind look.",
+    "imgsrc": "/images/img8.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Premium Quality & Craftsmanship",
+    "desc": "Made with high-grade, weather-resistant materials, our awnings guarantee durability and elegance, even in London's unpredictable climate.",
+    "imgsrc": "/images/img2.jpg"
+  },
+  {
+    "id": 4,
+    "title": "Expert Installation",
+    "desc": "Our skilled team ensures a perfect fit and seamless integration, providing optimal protection and enhancing your outdoor space with minimal disruption.",
+    "imgsrc": "/images/img15.jpg"
+  },
+]
+
 export default function Custom() {
+  
   return (
-    <FadeIn>
-      <section className="bg-[url('/banner-bg1.jpg')] my-20 h-[500px] bg-cover rounded-4xl relative">
-        <h1 className='text-4xl font-bold text-white absolute top-[50%] left-[50%] translate-x-[-50%] tracking-widest'>Custom Awnings in London</h1>
-        <p className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] text-white lg:flex hidden'>Enhance your property’s curb appeal with our premium custom awnings in London. Our service is designed for clients who value unique, tailor-made solutions. From initial design consultations to expert installation, we work closely with you to create awnings that perfectly complement your home or business aesthetic.</p>
-      </section>
-    </FadeIn>
+      <>
+        <Container className="mt-8 sm:mt-12 md:mt-16 mb-20">
+          <FadeIn className="max-w-3xl">
+            <h1 className="font-display text-4xl font-medium tracking-tight text-sky-950 [text-wrap:balance] sm:text-5xl">
+              Custom Awnings in London
+            </h1>
+            <p className="mt-2 text-xl text-sky-700">
+              Our service is designed for clients who value unique, tailor-made solutions. From initial design consultations to expert installation, we work closely with you to create awnings that perfectly complement your home or business aesthetic.
+            </p>
+          </FadeIn>
+        </Container>
+        <div className="mt-4 mx-2 rounded-4xl bg-sky-600 py-8 sm:mt-6 sm:py-10 lg:mt-6">
+        <Container>
+        <div className="flex items-center gap-x-2">
+            <h2 className="text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left">
+              Custom Awning Features
+            </h2>
+            <div className="h-px flex-auto bg-sky-500" />
+            </div>
+          <FadeInStagger faster>
+            <ul
+              role="list"
+              className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-x-2 gap-y-1 lg:grid-cols-3 text-white"
+              >
+              {CustomFeatures.map(([feature]) => (
+                <li key={feature} className={clsx('flex items-center ')}>
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </FadeInStagger>
+          </Container>
+        </div>
+        {CustomContent.map((content) => (
+          <Container key={content.id} className="mt-8 sm:mt-12 md:mt-16 mb-20">
+            <div className='flex justify-between items-center flex-col lg:flex-row '>
+              <FadeIn className="max-w-3xl gap-5">
+                <h2 className="font-display text-2xl font-medium tracking-tight text-sky-950 [text-wrap:balance] sm:text-5xl">
+                  {content.title}
+                </h2>
+                <p className="mt-2 text-lg text-sky-700 max-w-96">
+                  {content.desc}
+                </p>
+              </FadeIn>
+              <Image 
+                className='rounded-4xl'
+                alt='custom-canopy'
+                src={content.imgsrc}
+                width={500}
+                height={300}
+                />
+            </div>
+          </Container>
+          ))}
+          
+          
+      </>
   )
 }
--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -7,7 +7,7 @@ export default function CustomLink() {
     <section className='flex justify-center items-center gap-24 flex-col md:flex-row'>
         <div className='max-w-80 ml-5'>
           <h3 className='text-sky-800 text-3xl font-bold pb-2'>Custom Awning In London</h3>
-          <p className='pb-5'>Ready to transform your outdoor area with a truly bespoke awning? Contact us today to schedule a consultation and discover how our custom awning solutions can enhance your London property. Let’s create a space that’s as unique as you are.</p>
+          <p className='pb-5 text-sky-700'>Ready to transform your outdoor area with a truly bespoke awning? Contact us today to schedule a consultation and discover how our custom awning solutions can enhance your London property. Let’s create a space that’s as unique as you are.</p>
           <Link href={'/custom-awning-london'} className='px-8 py-3 bg-sky-600 text-white rounded-full'>Awning In London</Link>
         </div>
         <div>
