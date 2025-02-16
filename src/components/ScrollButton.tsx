d7ac5488e3edf09b8c3c825edc27ddfbcdd0edd1
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-02-16 19:23:13 +0330

    fix responsive

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -15,7 +15,7 @@ const ScrollButton: React.FC = () => {
   }, []);
 
   return (
-      <Link href={`tel:+44 7777 602125`} className={`fixed bottom-6 right-6 flex items-center justify-center bg-sky-600 text-white p-3 gap-2 rounded-full shadow-lg transition-opacity duration-300 hover:bg-sky-700 ${
+      <Link href={`tel:+44 7777 602125`} className={`fixed bottom-6 right-6 text-sm md:text-lg font-light flex items-center justify-center bg-sky-600 text-white p-3 gap-2 rounded-full shadow-lg transition-opacity duration-300 hover:bg-sky-700 ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}>
         <p>Free Phone Consultation</p>
