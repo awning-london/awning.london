8e2e3d26667f853372638f9e6914235f07e66585
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-03-04 15:18:17 +0330

    update alt image option

--- src/components/StylizedImage.tsx
+++ src/components/StylizedImage.tsx
@@ -42,7 +42,7 @@ export function StylizedImage({
           <g className="origin-center scale-100 transition duration-500 motion-safe:group-hover:scale-105">
             <foreignObject width={width} height={height}>
               <Image
-                alt=""
+                alt="image"
                 className="w-full bg-sky-100 object-cover"
                 style={{ aspectRatio: `${width} / ${height}` }}
                 {...props}
