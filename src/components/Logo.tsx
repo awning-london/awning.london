d15bc5cdfb44a2d96661c074b3f7037fe5e7cd65
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-05-15 22:24:15 +0330

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
-import Logo from '@/images/logo-awn.png'
+import Logo from '@/images/logo.png'
 
 export default function LogoV2() {
   return (
     <Image
         src={Logo}
-        width={40}
-        height={40}
+        width={50}
+        height={50}
         alt='logo of the awning'
-        className='md:w-14'
+        className=' md:w-14'
     />
   )
 }
