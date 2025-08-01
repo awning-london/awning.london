57fe93ddea4ad7ed13dcc9991b7380c85841f298
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-08-01 17:00:32 +0330

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 const nextConfig = {
-    output: 'export',
-    async rewrites() {
-        if (isGitHubPages) {
-        return [];
-        }
-        return [
-        {
-            source: '/sudio/:path*',
-            destination: '/sudio', // handled only outside GH Pages
-        },
-        ];
-    },
     images: {
         remotePatterns: [
             {
